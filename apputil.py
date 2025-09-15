2c1a8e4adecaba8d7afd6a419070e11f0d7da8d2
wprocise <wprocise@iu.edu>
2025-09-15 16:26:00 +0000

    Update to define fib

--- apputil.py
+++ apputil.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import pandas as pd
 
 # Exercise 1: Write a recursive function that returns the nth number of the Fibonacci Series
-# Define the fibonacci function
-def fibonacci(n):
+'''Define the fibonacci function
+fibonacci = fib_recursive'''
+def fib_recursive(n):
 

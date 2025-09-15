be9a71f49d6404fb656d17ee5cb66405171ae875
wprocise <wprocise@iu.edu>
2025-09-15 16:16:59 +0000

    Define function

--- apputil.py
+++ apputil.py
@@ -1,5 +1,7 @@
 import seaborn as sns
 import pandas as pd
 
+# Exercise 1: Write a recursive function that returns the nth number of the Fibonacci Series
+# Define the fibonacci function
+def fibonacci(n):
 
-# update/add code below ...

16fd91c58fa3d2693d3daa852ba07dd40577233c
wprocise <wprocise@iu.edu>
2025-09-15 16:37:28 +0000

    If-elif-else statement

--- apputil.py
+++ apputil.py
@@ -5,4 +5,12 @@ import pandas as pd
 '''Define the fibonacci function
 fibonacci = fib_recursive'''
 def fib_recursive(n):
+# Using an if-elif-else statement, find fib_recursive starting with 0 and 1
+# If n is 0, return 0; if n is 1, return 1
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_recursive(n-1) and fib_recursive(n-2)
 

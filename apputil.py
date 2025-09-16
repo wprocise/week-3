cb36ad9ab99b650fcc4cdd5481856949affc84ae
wprocise <wprocise@iu.edu>
2025-09-16 16:35:40 +0000

    Replaced ('age') with ['age'] to get correct output for pandas series

--- apputil.py
+++ apputil.py
@@ -79,7 +79,7 @@ def task_iii():
     # Create the series with index
     df = pd.DataFrame(df_bellevue)
     # Group the DataFrame by 'gender' and calculate the mean age for males and females using groupby.mean()
-    avg_age = df_bellevue.groupby('gender')('age').mean()
+    avg_age = df_bellevue.groupby('gender')['age'].mean()
     return avg_age
 # Test the function task_iii
 print(task_iii())
